a08a198c462de85c7702f4ad2ac847a6f695007a
Danil <danilx02@yandex.ru>
2024-10-15 16:51:12 +0300

    fix bugs and create auth for client

--- client/network.py
+++ client/network.py
@@ -1,32 +1,70 @@
+import json
 import socket
 
-from .config import SERVER_IP, SERVER_PORT, CHUNK
+from config import SERVER_IP, SERVER_PORT, CHUNK
 
 
 class NetworkClient:
     def __init__(self):
         self.sock = None
+        self.is_connected = False
 
     def connect(self):
         """Подключение к серверу."""
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((SERVER_IP, SERVER_PORT))
-        print(f"Connected to server at {SERVER_IP}:{SERVER_PORT}")
+        if not self.is_connected:
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.connect((SERVER_IP, SERVER_PORT))
+                self.is_connected = True
+            except socket.error as e:
+                self.is_connected = False
 
     def disconnect(self):
         """Отключение от сервера."""
         if self.sock:
             self.sock.close()
             self.sock = None
+            self.is_connected = False
             print("Disconnected from server")
 
     def send_data(self, data):
         """Отправка данных на сервер."""
-        if self.sock:
-            self.sock.sendall(data)
+        if self.is_connected:
+            try:
+                self.sock.sendall(data)
+            except socket.error as e:
+                print(f"Failed to send data: {e}")
+                self.disconnect()
 
     def receive_data(self):
         """Получение данных от сервера."""
-        if self.sock:
-            return self.sock.recv(CHUNK)
+        if self.is_connected:
+            try:
+                return self.sock.recv(CHUNK)
+            except socket.error as e:
+                print(f"Failed to receive data: {e}")
+                self.disconnect()
         return None
+
+    def send_request(self, request_type, username, password):
+        """Отправляет запрос на сервер и получает ответ."""
+        if not self.is_connected:
+            self.connect()
+
+        if self.is_connected:
+            message = {
+                "type": request_type,
+                "username": username,
+                "password": password
+            }
+            request_json = json.dumps(message).encode('utf-8')
+
+            # Отправка данных на сервер
+            self.send_data(request_json)
+
+            # Получаем ответ от сервера
+            response = self.receive_data()
+            return json.loads(response.decode('utf-8')) if response else None
+        else:
+            print("Unable to send request, no connection to server.")
+            return None
--- server/server.py
+++ server/server.py
@@ -1,15 +1,21 @@
 import asyncio
 
-from .config import SERVER_HOST, SERVER_PORT
-from .network import handle_client
+from config import SERVER_HOST, SERVER_PORT
+from network import handle_client, get_db_connection
 
 
 async def main():
-    server = await asyncio.start_server(handle_client, SERVER_HOST, SERVER_PORT)
+    db_conn = get_db_connection()
+    if db_conn is None:
+        print("Failed to connect to the database. Exiting.")
+        return
+
+    server = await asyncio.start_server(lambda r, w: handle_client(r, w, db_conn), SERVER_HOST, SERVER_PORT)
     async with server:
         print(f"Server started on {SERVER_HOST}:{SERVER_PORT}")
         await server.serve_forever()
 
+    db_conn.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
